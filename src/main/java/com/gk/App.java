ba6de59f597ff6e5c8c2d8f6deec9b548ac98fef
wb-gaokuo <goakuo_ok@163.com>
2018-02-22 12:52:53 +0800

    静态，非静态，方法，匿名内部类实现

--- src/main/java/com/gk/App.java
+++ src/main/java/com/gk/App.java
@@ -6,8 +6,19 @@ package com.gk;
  */
 public class App 
 {
+
+    public final transient static  int a =3;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        new App(){
+          void fun(){
+              System.out.println("匿名内部类的fun方法");
+          }
+        }.fun();
+
+
+
     }
 }
